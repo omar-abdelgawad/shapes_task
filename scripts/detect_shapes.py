a991586a0fe6dc478bcca2c2f05e437be1ae9e07
omar-abdelgawad <omar.abdelgawad@ejust.edu.eg>
2023-10-02 13:25:34 +0300

    added detect_shapes module

---
+++ scripts/detect_shapes.py
@@ -0,0 +1,40 @@
+"""This module contains functions for detecting shapes in an image."""
+from ultralytics import YOLO
+from typing import Optional
+from typing import Sequence
+import numpy as np
+import os
+
+dir_path = os.path.dirname(__file__)
+model_path = os.path.join(dir_path, "shapes_model", "last.pt")
+model = YOLO(model_path)
+
+
+def calculate_score(results):
+    shape_score_dict = {"circle": 20, "square": 15, "triangle": 10, "cross": 5}
+    total_score = 0
+    shapes_conversion_dict = results.names
+    for obj in map(int, results.cpu().boxes.cls.int()):
+        shape = shapes_conversion_dict[obj]
+        total_score += shape_score_dict[shape]
+    return total_score
+
+
+def detect_shapes(img: np.ndarray) -> int:
+    """Detects shapes in an image.
+
+    Args:
+        image: A numpy array representing an image.
+
+    Returns:
+        score(int):  representing the score of the shape detected.
+    """
+    raise NotImplementedError
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    main(None)
